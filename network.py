8c222e89479bf72118e97ac210e774d0afde01ad
Jano <jano.thaler@gmx.at>
2025-05-28 10:51:29 +0200

    Added game file and modified network file

--- network.py
+++ network.py
@@ -4,7 +4,7 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.210.59"#"10.0.0.18""192.168.30.209"#"192.168.30.59"#
+        self.server = "192.168.178.1"#"10.0.0.18""192.168.30.209"#"192.168.30.59"#
         self.port = 5555
         self.addr = (self.server, self.port)
         self.p = self.connect()
