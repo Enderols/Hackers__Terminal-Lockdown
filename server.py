5a5e1f5b324178b75bb65378f4d567865ae182f2
Jano <jano.thaler@gmx.at>
2025-06-16 17:46:54 +0200

    reset button added

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import tkinter as tk
 
 global gameStart
 gameStart = False
-server = "192.168.0.1"
+server = "172.22.0.1"
 port = 5555
 
 def threaded_client(conn, playerId):
@@ -17,30 +17,29 @@ def threaded_client(conn, playerId):
         pass
     conn.send(pickle.dumps(len(players)))
     while True:
+        if gameStart == False:
+            print("Game Stopped")
+            players[playerId] = None
+            if all(p is None for p in players):
+                print("All clients disconnected. Clearing players list.")
+                players.clear()
+                currentPlayer = 0
+            break
         try:
             data = pickle.loads(conn.recv(2048))
             players[playerId] = data
 
            
-            
             if not data: 
                 print("Disconnected")
                 break
-            else:
-            
-                if players[playerId]["givendamage"]:
-                    print(players[playerId]["givendamage"])
-                    for key in players[playerId]["givendamage"]:
-                        players[key]["health"] = players[playerId]["givendamage"][key]
-                        print(f"Player {key} health updated to {players[key]['health']}")
-                        
+            else:                        
 
-            reply = players.copy()  # Send a copy of the players list to all clients
+                reply = players.copy()  # Send a copy of the players list to all clients
                 
                 #print("Received: ", data)
                 #print("Sending: ", reply)
-            print(f"health von spieler {playerId}: {reply[playerId]["health"]}")
-            conn.sendall(pickle.dumps(reply))
+                conn.sendall(pickle.dumps(reply))
             
         except:
             print("Error")
@@ -58,13 +57,19 @@ def start_button_clicked():
     global gameStart
     gameStart = True
 
+def reset_button_clicked():
+    global gameStart
+    gameStart = False
+
 def checkForGameStart():
     # Create the main window
     strtwin = tk.Tk()
     strtwin.title("Start Game")
     # Create and pack the start button
-    start_button = tk.Button(strtwin, text="Start", command=start_button_clicked)
-    start_button.pack(pady=20)
+    startButton = tk.Button(strtwin, text="Start", command=start_button_clicked)
+    resetButton = tk.Button(strtwin, text="Reset", command=reset_button_clicked)
+    startButton.pack(pady=20)
+    resetButton.pack(pady=20)
     strtwin.mainloop()
        
 
